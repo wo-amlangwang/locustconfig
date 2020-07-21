bf12ae836fedbf0a710886a3a247f84fec650d9b
Fangzhou Lin <adamlangwang@gmail.com>
2020-07-21 12:47:36 +0800

    Fix query

--- common/parser.py
+++ common/parser.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import urllib.parse as urlparse
+from urllib.parse import urlencode
 
 DEFAULT_METHOD_INDEX = 0
 DEFAULT_URL_INDEX = 1
@@ -42,6 +44,14 @@ def post_function(url, header, data):
     return lambda session: session.client.post(url=url, headers=header, data=json.dumps(data))
 
 
+def append_query(url, querys):
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(querys)
+    url_parts[4] = urlencode(query)
+    return urlparse.urlunparse(url_parts)
+
+
 class Parser:
     def __init__(self):
         self.reqDict = {}
@@ -58,11 +68,14 @@ class Parser:
             url = elements[DEFAULT_URL_INDEX]
             next_index = DEFAULT_URL_INDEX + 1
             next_index, header = concat_json_string(elements, next_index)
-            next_index, _ = concat_json_string(elements, next_index)
+            next_index, query = concat_json_string(elements, next_index)
             _, body = concat_json_string(elements, next_index)
             parsed_header = json.loads(header)
+            parsed_query = json.loads(query)
             parsed_body = json.loads(body)
+            url = append_query(url, parsed_query)
             if method == "GET":
                 self.reqDict[get_function(url, parsed_header)] = HIGH_PRIORITY
             if method == "POST":
                 self.reqDict[post_function(url, parsed_header, parsed_body)] = LOW_PRIORITY
+
