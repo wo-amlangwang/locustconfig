40f020a80e0cf7ae50c39aaf385c6b510f9b37de
Fangzhou Lin <fanglin@microsoft.com>
2020-07-30 16:12:47 +0800

    fix

--- common/my_fasthttp.py
+++ common/my_fasthttp.py
@@ -4,6 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class MyFastHttpUser(FastHttpUser):
 
     def __init__(self, environment):
--- locustconfig.py
+++ locustconfig.py
@@ -9,7 +9,7 @@ def get_tests():
     return parse(getcwd() + "/access.log")
 
 
-class MyUser(common.my_fasthttp.MyUser):
+class MyUser(common.my_fasthttp.MyFastHttpUser):
     min_wait = 100
     max_wait = 120
     tasks = get_tests()
