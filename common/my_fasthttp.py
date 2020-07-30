49d8d6db14691faebec42b681271fbb4e209f792
Fangzhou Lin <fanglin@microsoft.com>
2020-07-30 15:55:55 +0800

    fix

--- common/my_fasthttp.py
+++ common/my_fasthttp.py
@@ -6,7 +6,7 @@ class MyUser(FastHttpUser):
 
     def get(self, path, headers, **kwargs):
         """Sends a GET request"""
-        response = self.request("GET", path, headers=headers, **kwargs)
+        response = super(MyUser,self).request("GET", path, headers=headers, **kwargs)
         print(1)
         try:
             response.raise_for_status()
